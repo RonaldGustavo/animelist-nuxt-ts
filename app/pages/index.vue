90451a200b5f7afe49f30e65a66cd739fa2f96f1
Ronald Gustavo <Ronald@dbo.id>
2025-08-04 16:40:24 +0700

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+        <p>bro</p>
+        <p>bro</p>
+        <p>bro</p>
+        <p>bro</p>
+        <p>bro</p>
+        <p>bro</p>
+    </template>
+
